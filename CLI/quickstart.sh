a2f1aad892995c2ccd140fef0130840a04aabef1
Moody Muggle <61803412+ranipm2705@users.noreply.github.com>
2021-05-06 17:50:57 +0530

    Create quickstart.sh

---
+++ CLI/quickstart.sh
@@ -0,0 +1,23 @@
+alias k='kubectl'
+alias c='clear'
+
+export dy="--dry-run=client -o yaml"
+export all="--all-namespaces"
+export fgp="--force --grace-period=0"
+
+alias kg='kubectl get'
+alias kd='kubectl describe'
+alias kr='kubectl run'
+alias kc='kubectl create'
+alias kdel='kubectl delete'
+alias ke='kubectl explain'
+
+alias kgn='kubectl get no'
+alias kgp='kubectl get po'
+alias kgs='kubectl get svc'
+alias kgd='kubectl get deploy'
+alias kgcm='kubectl get cm'
+alias kgsa='kubectl get sa'
+alias kgsec='kubectl get secrets'
+
+alias kga='kubectl get all'
